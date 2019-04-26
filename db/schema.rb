82387b950a85505b027a777ae8dc92d1fd1dadcb
Leah Plat <leahplat@gmail.com>
2019-04-25 23:52:27 -0400

    fixed typos

--- db/schema.rb
+++ db/schema.rb
@@ -15,20 +15,6 @@ ActiveRecord::Schema.define(version: 20190426001438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "quot", force: :cascade do |t|
-    t.string   "saying"
-    t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "quotations", force: :cascade do |t|
-    t.string   "saying"
-    t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "quotes", force: :cascade do |t|
     t.string   "saying"
     t.string   "author"
